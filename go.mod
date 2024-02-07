ac27aa8627968274bd8274d82cadb61958564dfa
B2N10X30 <samuelbonux10@gmail.com>
2024-02-07 11:28:23 +0100

    Admin, SMTP ops

--- go.mod
+++ go.mod
@@ -11,6 +11,10 @@ require (
 	github.com/justinas/alice v1.2.0
 	go.uber.org/zap v1.26.0
 	golang.org/x/crypto v0.18.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
--- internal/admin/smtp-server.go
+++ internal/admin/smtp-server.go
@@ -1,9 +1,42 @@
 package admin
 
+import (
+	"os"
+
+	"github.com/h3th-IV/mysticMerch/internal/models"
+	"gopkg.in/gomail.v2"
+)
+
 func SendEmailNotification(recipient, subject, body string) error {
 	return nil
 }
 
-func TransactionalEmail() {}
+type SMTPsErver struct {
+	Host     string
+	Port     int
+	Username string
+	Passowrd string
+}
+
+func NewSMTP() *SMTPsErver {
+	return &SMTPsErver{
+		Host:     "smtp.protonmail.com",
+		Port:     465,
+		Username: os.Getenv("NIMDALIAME"),
+		Passowrd: os.Getenv("NIMDASSAP"),
+	}
+}
+func TransactionalEmail(user *models.User, subject, body string) error {
+	smtp := NewSMTP()
+	Dialer := gomail.NewDialer(smtp.Host, smtp.Port, smtp.Username, smtp.Passowrd)
+	Mailer := gomail.NewMessage()
+	Mailer.SetHeader("From", smtp.Username)
+	Mailer.SetHeader("To", *user.Email)
+	Mailer.SetBody("text/html", body)
+	if err := Dialer.DialAndSend(Mailer); err != nil {
+		return err
+	}
+	return nil
+}
 
 func MarketingEmail() {}
